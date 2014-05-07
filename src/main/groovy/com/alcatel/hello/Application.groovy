e3f2955bc396c60bb8c46d3d9983a08e735004db
Guoshun Wu <Guoshun Wu@localhost>
2014-05-07 11:18:08 +0800

    add groovy support add gorm support

---
+++ src/main/groovy/com/alcatel/hello/Application.groovy
@@ -0,0 +1,22 @@
+package com.alcatel.hello
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.security.SecurityProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
+
+@ComponentScan
+@EnableAutoConfiguration
+class Application {
+    static void main(String[] args) {
+        SpringApplication.run Application, args
+    }
+
+    @Bean
+    SecurityProperties securityProperties() {
+        SecurityProperties security = new SecurityProperties()
+        security.basic.path = "" // empty so home page is unsecured
+        security
+    }
+}
---
+++ src/main/groovy/com/alcatel/hello/controller/GGreetingController.groovy
@@ -0,0 +1,33 @@
+package com.alcatel.hello.controller
+
+import com.alcatel.hello.domain.Person
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST
+import static org.springframework.http.HttpStatus.CREATED
+import static org.springframework.web.bind.annotation.RequestMethod.GET
+import static org.springframework.web.bind.annotation.RequestMethod.POST
+
+/**
+ * Created by guoshunw on 2014/5/7.
+ */
+
+@RestController
+class GGreetingController {
+    @RequestMapping(value = "/person/greet", method = GET)
+    String greet(String firstName) {
+        Person p = Person.findByFirstName(firstName)
+        p ? "Hello ${p.firstName}!" : "Person ${p.firstName} not found"
+    }
+
+    @RequestMapping(value = "/person/add", method = POST)
+    ResponseEntity<Person> addPerson(String firstName, String lastName) {
+        Person.withTransaction {
+            Person p = new Person(firstName: firstName, lastName: lastName).save()
+            new ResponseEntity(p ? CREATED : BAD_REQUEST)
+        }
+    }
+}
+

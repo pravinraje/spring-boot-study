0468b9b1abaf3440972ede7475b3d4e0ecd83166
Guoshun Wu <Guoshun Wu@localhost>
2014-05-08 09:55:13 +0800

    move entities to groovy

--- src/main/groovy/com/alcatel/hello/controller/GreetingController.groovy
+++ src/main/groovy/com/alcatel/hello/controller/GreetingController.groovy
@@ -1,10 +1,17 @@
 package com.alcatel.hello.controller
 
+import com.alcatel.hello.domain.Greeting
+import com.alcatel.hello.domain.HelloMessage
 import com.alcatel.hello.domain.Person
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.ResponseEntity
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+import static java.util.concurrent.TimeUnit.SECONDS
 import static org.springframework.http.HttpStatus.BAD_REQUEST
 import static org.springframework.http.HttpStatus.CREATED
 import static org.springframework.web.bind.annotation.RequestMethod.GET
@@ -15,7 +22,10 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST
  */
 
 @RestController
-class GGreetingController {
+class GreetingController {
+
+    private static Logger log = LoggerFactory.getLogger(GreetingController)
+
     @RequestMapping(value = "/person/greet", method = GET)
     String greet(String firstName) {
         Person p = Person.findByFirstName(firstName)
@@ -28,6 +38,17 @@ class GGreetingController {
             Person p = new Person(firstName: firstName, lastName: lastName).save()
             new ResponseEntity(p ? CREATED : BAD_REQUEST)
         }
+    }
+
+    /**
+     * For the web socket message back
+     * */
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        log.info("greeting with message {}", message)
+        SECONDS.sleep(1)
+        new Greeting("Hello, " + message.getName() + "!")
     }
 }
 
---
+++ src/main/groovy/com/alcatel/hello/domain/Greeting.groovy
@@ -0,0 +1,12 @@
+package com.alcatel.hello.domain
+
+/**
+ * Created by guoshunw on 2014/5/8.
+ */
+class Greeting {
+
+    Greeting(String content) {
+        this.content = content
+    }
+    String content
+}
